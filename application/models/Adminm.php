3844a5202134ae94fb82126933424ec1b95823e4
stevezaxz <nevetsjohn@gmail.com>
2015-03-24 09:18:17 +0800

    2015-25-03 9:18am

--- application/models/Adminm.php
+++ application/models/Adminm.php
@@ -20,4 +20,36 @@ class Adminm extends CI_Model {
         }
     }
 
+    public function getcompanydetails($company_id) {
+        $array = array();
+        $query = $this->db->query("select * From tblcompany where company_id ='$company_id' ");
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $value) {
+                $array = array(
+                    'company_registrants_name' => $value->company_registrants_name,
+                    'company_username' => $value->company_username,
+                    'company_trade_name' => $value->company_trade_name,
+                    'company_tin' => $value->company_tin,
+                    'company_bir_registration_no' => $value->company_bir_registration_no,
+                    'company_website' => $value->company_website,
+                    'company_history' => $value->company_history
+                );
+            }
+            return $array;
+        } else
+            return false;
+    }
+
+    public function approvecompany($company_id) {
+        $this->db->trans_begin();
+        $query = $this->db->query("update tblcompany set is_verified ='1' where company_id = '$company_id' ");
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+            return false;
+        } else {
+            $this->db->trans_commit();
+            return true;
+        }
+    }
+
 }
--- application/views/requestreg.php
+++ application/views/requestreg.php
@@ -1,6 +1,10 @@
-
+<div class="alert alert-info" role="alert" style="width:300px;" id="hidden">
+    <!--<button  type="button" class="close" data-dismiss="alert" id="close">&times;</button>-->
+    <center><strong id="result"></strong> </center>
+</div>
 <div id="page-wrapper">
     <div class="row">
+
         <div class="col-lg-12">
             <h1 class="page-header">Dashboard</h1>
         </div>
@@ -8,6 +12,7 @@
     </div>
     <!-- /.row -->
     <div class="row">
+
         <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
@@ -55,7 +60,7 @@
 //                                        echo "<td>" . $value['company_website'] . "</td>";
                                         echo "<td ><button class='approve btn btn-success btn-sm' value='" . $value['company_id'] . "'><span class='glyphicon glyphicon-ok'></span></button></td>";
                                         echo "<td ><button class='deny btn btn-primary btn-sm' value='" . $value['company_id'] . "'><span class='glyphicon glyphicon-remove'></span></button></td>";
-                                        echo "<td ><button class='details btn btn-success btn-sm' value='" . $value['company_id'] . "'>View Details</button></td>";
+                                        echo "<td ><button href='#myModal' data-toggle='modal' class='details btn btn-success btn-sm' value='" . $value['company_id'] . "'>View Details</button></td>";
                                         echo "</tr>";
                                     }
                                 }
@@ -72,22 +77,147 @@
     <!-- /.row -->
     <div class="row">
 
-        <!-- /.col-lg-8 -->
+        <!--<a href="#myModal" class="btn btn-lg btn-primary" data-toggle="modal">Launch Demo Modal</a>-->
 
-        <!-- /.col-lg-4 -->
+        <!-- Modal HTML -->
+        <div id="myModal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Confirmation</h4>
+                    </div>
+                    <div class="modal-body">
+<!--                        <p>Do you want to save changes you made to document before closing?</p>
+                        <p class="text-warning"><small>If you don't save, your changes will be lost.</small></p>-->
+                        <form class="form-horizontal">
+                            <fieldset>
+
+                                <!-- Form Name -->
+                                <h4>  Company Details</h4>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_registrants_name">Registrants Name</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_registrants_name" name="company_registrants_name" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_username">Username</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_username" name="company_username" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_trade_name">Trade Name</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_trade_name" name="company_trade_name" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_tin">TIN</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_tin" name="company_tin" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_bir_registration_no">BIR Registration No</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_bir_registration_no" name="company_bir_registration_no" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_website">Website</label>  
+                                    <div class="col-md-6">
+                                        <input disabled id="company_website" name="company_website" type="text" placeholder="" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- Textarea -->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="company_history">Company History</label>
+                                    <div class="col-md-6">                     
+                                        <textarea  cols="10" rows="10" disabled class="form-control" id="company_history" name="company_history"></textarea>
+                                    </div>
+                                </div>
+
+                            </fieldset>
+                        </form>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <!--<button type="button" class="btn btn-primary">Close</button>-->
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
     <!-- /.row -->
 </div>
+
 <!-- /#page-wrapper -->
 <script type="text/javascript">
-    $(".approve").click(function() {
-        alert($(this).val());
+    $(".approve").click(function(e) {
+//        alert($(this).val());
+
+        $.post("<?php echo site_url("Adminc/approvecompany"); ?>", {company_id: $(this).val()}, function(res) {
+            //automatic ref div
+//              window.location.reload();
+            if (res == 'success') {
+//                alert(1);
+                $("#result").text("Company registration details approved");
+                $("#hidden").fadeIn(5000);
+                $("#hidden").fadeOut(5000).delay(800, function() {
+                    location.reload();
+                });
+
+//                window.location.reload();
+            }
+            else {
+
+            }
+        });
+//        window.location.reload();
     });
     $(".deny").click(function() {
         alert($(this).val());
     });
     $(".details").click(function() {
-        alert($(this).val());
+        $.post("<?php echo site_url("Adminc/getcompanydetails"); ?>", {company_id: $(this).val()}, function(json) {
+            jsonstring = jQuery.parseJSON(json);
+            $("#company_registrants_name").val(jsonstring.company_registrants_name);
+            $("#company_username").val(jsonstring.company_username);
+            $("#company_trade_name").val(jsonstring.company_trade_name);
+            $("#company_tin").val(jsonstring.company_tin);
+            $("#company_bir_registration_no").val(jsonstring.company_bir_registration_no);
+            $("#company_website").val(jsonstring.company_website);
+            $("#company_history").val(jsonstring.company_history);
+        });
     });
 
 </script>
+
+<style type="text/css">
+    div#hidden.alert.alert-info{
+        display:none;
+</style> 
+
